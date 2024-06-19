eb9ccc32055896977d397a95481174823c19a2b4
MagnoDazaZuleta <magnodaza@hotmail.com>
2024-06-18 23:52:30 -0500

    mas o mierda

---
+++ lib/botones/button_mixins.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+//mixin para color de texto y color de fondo para elevatedbutton
+mixin ELevatedButtonMixin {
+  Color get textColor => Colors.black;
+  Color get color => Colors.white;
+}

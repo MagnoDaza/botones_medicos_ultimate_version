dea83db9034c5e34e22b1e84ac0c2753bb182828
MagnoDazaZuleta <magnodaza@hotmail.com>
2024-07-14 21:54:28 -0500

    Ahora se puede cambiar el tipo de boton y se migran los cambios que tiene el boton

--- lib/Screen/page_screen.dart
+++ lib/Screen/page_screen.dart
@@ -140,24 +140,23 @@ class ButtonPageState extends State<ButtonPage> {
     }
   }
 
-  Future<void> _selectButtonType() async {
+ Future<void> _selectButtonType() async {
     final selectedIndex = await Navigator.push<int>(
       context,
       MaterialPageRoute(
         builder: (context) => GridPage(
           buttonModel: Provider.of<ButtonModel>(context, listen: false),
-          selectedButtonType:
-              selectedButtonType, // Pasar el tipo de botón seleccionado
+          selectedButtonType: selectedButtonType,
         ),
       ),
     );
     if (selectedIndex != null) {
       setState(() {
         final buttonModel = Provider.of<ButtonModel>(context, listen: false);
-        selectedButtonType = buttonModel.factoryButtons[selectedIndex].type;
-        buttonModel.selectButton(selectedIndex);
-        _buttonTextController.text =
-            buttonModel.factoryButtons[selectedIndex].text;
+        final newType = buttonModel.factoryButtons[selectedIndex].type;
+        buttonModel.moveButtonDataToNewType(newType);
+        selectedButtonType = newType;
+        _buttonTextController.text = buttonModel.temporaryButton?.text ?? '';
       });
     }
   }
